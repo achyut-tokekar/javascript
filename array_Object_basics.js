aee79752cc5f74ac604f522d68389185701cb4cb
Achyut Tokekar <achyut.tokekar@pw.live>
2022-12-25 20:37:02 +0530

    initial commit

---
+++ array_Object_basics.js
@@ -0,0 +1,38 @@
+
+/**
+ * Array Basics
+ */
+
+const arr2 = [];
+const arr = [12, "dsf", true];
+const arr3 = [1, 2, 3, 4, 5];
+
+const array = ["Monday", "Tuesday", "Wednesday" ];
+
+const detail_2 = ["Varsha", "Female"];
+
+
+console.log(array[1])
+
+
+
+/**
+ * Object
+ */
+
+
+const obj = { 
+    key: "value"
+};
+
+const detail = {
+  name: "Varsha",
+  gender: "female"
+};
+
+// Way to access object keys 
+console.log(detail['gender']);
+        OR
+console.log(detail.name);
+
+
---
+++ conditional_operators.js
@@ -0,0 +1,33 @@
+
+
+
+/**
+ * Conditional Operators
+ */
+
+// IF-ELSE
+const a = 3
+
+if( a > 2) {
+  console.log("pehla wala");
+} else if( a < 2) {
+  console.log("dusra wala");
+} else {
+  console.log("equal wala")
+}
+
+// SWITCH-CASE
+
+let fruit = 'Apple';
+
+switch(fruit) {
+  case 'Mango':
+    console.log("Mango is fruit");
+    break;
+  case "Apple":
+    console.log("Apple is fruit");
+    break;
+  default:
+    console.log("DEFAULT!!");
+}
+
